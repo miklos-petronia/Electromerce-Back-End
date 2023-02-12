d81e8bc2fabca4be0c4d91e0cf8cc41e1c00e0d1
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-13 00:46:39 +0100

    tag name column to macth the model

--- models/tag.js
+++ models/tag.js
@@ -14,3 +14,19 @@ Tag.init(
             primaryKey: true,
             autoIncrement: true,
         },
+        // Tag name column to match model
+        tag_name: {
+            type: DataTypes.STRING
+        }
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag',
+    }
+);
+
+module.exports = Tag;
