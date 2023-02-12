5d7b78be930ab69b01bd7a9fb52ba689519ce82b
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-12 04:00:41 +0100

    added sequilize timsestamps, freesetablename, underscoed and modelname

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -35,3 +35,12 @@ ProductTag.init(
         }
     },
     {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag',
+    }
+);
+
+module.exports = ProductTag;
