45c96ce6871472be0034c388ed8b3d1e1877e501
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-13 16:19:20 +0100

    developing new product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,3 +34,26 @@ router.get('/:id', async (req, res) => {
         res.json(err);
     }
 });
+
+// create new product
+router.post('/', (req, res) => {
+    /* req.body should look like this...
+      {
+        product_name: "Basketball",
+        price: 200.00,
+        stock: 3,
+        tagIds: [1, 2, 3, 4]
+      }
+    */
+    Product.create(req.body)
+        .then((product) => {
+            // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+            if (req.body.tagIds.length) {
+                const productTagIdArr = req.body.tagIds.map((tag_id) => {
+                    return {
+                        product_id: product.id,
+                        tag_id,
+                    };
+                });
+                return ProductTag.bulkCreate(productTagIdArr);
+            }
