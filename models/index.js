b2946cd9dfb993c1365f21a19d6f377e4f452c0f
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-13 01:27:57 +0100

    Tags belong to many products within the product tag

--- models/index.js
+++ models/index.js
@@ -10,10 +10,24 @@ Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong To Many Tags (through ProductTag)
 Product.belongsToMany(Tag, {
     through: {
         model: ProductTag,
         foreignKey: 'product_id'
     }
 })
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: {
+        model: ProductTag,
+        foreignKey: 'tag_id'
+    }
+})
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+};
