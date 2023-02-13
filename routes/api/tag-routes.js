9ebc55c02f8f357ab47092661d2c9631252c8bc2
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-13 17:26:44 +0100

    Including associated product information

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+    try {
+        // find all tags
+        // be sure to include its associated Product data
+        const getAllTags = await Tag.findAll({
+            attributes: ["id", "tag_name"],
+            include: [{
+                model: Product,
+                attributes: ["id", "product_name", "price", "stock", "category_id"],
+                through: "ProductTag",
+            }],
+        });
+        res.json(getAllTags);
+    } catch (err) {
+        res.json(err);
+    }
+});
