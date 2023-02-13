daedcaea6d6ee61aa426877ad070ccf4e3ef7260
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-13 17:28:37 +0100

    including the associated product information

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,3 +20,19 @@ router.get('/', async (req, res) => {
         res.json(err);
     }
 });
+router.get('/:id', async (req, res) => {
+    try {
+        // find a single tag by its `id`
+        // be sure to include its associated Product data
+        const getTagById = await Tag.findByPk(req.params.id, {
+            include: [{
+                model: Product,
+                attributes: ["id", "product_name", "price", "stock", "category_id"],
+                through: "ProductTag",
+            }],
+        });
+        res.json(getTagById);
+    } catch (err) {
+        res.json(err);
+    }
+});
