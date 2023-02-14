796c8e8ff41a738783f5e88bbdb6671da951ab0c
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-14 18:24:00 +0100

    updated index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,17 @@
-// import models
+
+// Get frameworks
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongs to Category
+// Products resides to Category
 Product.belongsTo(Category);
 
 // Categories have many Products
 Category.hasMany(Product);
 
-// Products belong To Many Tags (through ProductTag)
+// Products resides To Many Tags (through ProductTag)
 Product.belongsToMany(Tag, {
     through: {
         model: ProductTag,
@@ -18,7 +19,7 @@ Product.belongsToMany(Tag, {
     }
 })
 
-// Tags belong To Many Products (through ProductTag)
+// Tags resides To Many Products (through ProductTag)
 Tag.belongsToMany(Product, {
     through: {
         model: ProductTag,
